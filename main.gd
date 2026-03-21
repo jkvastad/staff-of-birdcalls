e64c9f0eb0c165e54287f8b407896dfbb8bb5080
jkvastad <jkvastad@gmail.com>
2026-03-21 20:08:28 +0100

    dynamically load birds from name list

--- main.gd
+++ main.gd
@@ -1,11 +1,17 @@
 extends Control
 
-@onready var button = $VBoxContainer/Button
 @onready var audio_player = $AudioStreamPlayer2D
+var bird_names = ["Finch", "Raven"]
+
+@onready var vbox_container = $VBoxContainer/BirdButtonContainer
 
 func _ready():
-	button.pressed.connect(on_button_pressed)
+	for bird in bird_names:
+		var bird_button = TextureButton.new()
+		bird_button.texture_normal = load("res://Birdcalls/" + bird + "/icon.webp")
+		bird_button.pressed.connect(on_button_pressed.bind(bird))
+		vbox_container.add_child(bird_button)
 	
-func on_button_pressed():
-	audio_player.stream = preload("res://Birdcalls/Finch/house-finch-song-1666.mp3")
+func on_button_pressed(bird_name: String):
+	audio_player.stream = load("res://Birdcalls/" + bird_name + "/sound.mp3")
 	audio_player.play()
