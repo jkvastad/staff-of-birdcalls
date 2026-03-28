66a126e7c8ac161a837b1f383a2fad59b6e7bd5b
jkvastad <jkvastad@gmail.com>
2026-03-28 11:58:38 +0100

    Added sound and icon files Fixed icon aspect scaling

---
+++ README.md
@@ -0,0 +1,3 @@
+adb devices
+cd to folder containing StaffOfBirdcalls.apk
+adb -s <Your device number here> install -r StaffOfBirdcalls.apk
--- main.gd
+++ main.gd
@@ -7,16 +7,17 @@ const bird_names = ["Finch","Raven","Duck","Chicken","Goose","Loon","Turkey","Gu
 @onready var bird_container = $VBoxContainer/ScrollContainer/BirdButtonContainer
 
 func _ready():
-	var icon_size = get_viewport().get_visible_rect().size	
+	var icon_size = get_viewport_rect().size	
 	for bird_name in bird_names:
 		var bird_button = TextureButton.new()
 		# allow mouse input to pass through buttons, enabling touch scroll
 		bird_button.mouse_filter = Control.MOUSE_FILTER_PASS
 		bird_button.stretch_mode = TextureButton.STRETCH_SCALE
-		bird_button.custom_minimum_size = Vector2(icon_size.x/2,icon_size.x/2)
+		bird_button.custom_minimum_size = Vector2(icon_size.x/2,icon_size.y/6)
 		bird_button.ignore_texture_size = true
 		bird_button.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
 		bird_button.texture_normal = load("res://Birdcalls/" + bird_name + "/icon.webp")
+		bird_button.stretch_mode = TextureButton.STRETCH_KEEP_ASPECT_COVERED
 		bird_button.pressed.connect(on_button_pressed.bind(bird_name))
 		bird_container.add_child(bird_button)
 	
