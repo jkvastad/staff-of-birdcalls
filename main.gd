9b46166f15928ca6f2b2cdd99ee4ea44fad177f9
jkvastad <jkvastad@gmail.com>
2026-03-28 11:15:38 +0100

    Bird buttons now display as two full columns Added touch scroll

--- main.gd
+++ main.gd
@@ -1,16 +1,24 @@
 extends Control
 
 @onready var audio_player = $AudioStreamPlayer2D
-var bird_names = ["Finch", "Raven"]
+@onready var scroll_container = $VBoxContainer/ScrollContainer
+const bird_names = ["Finch","Raven","Duck","Chicken","Goose","Loon","Turkey","Gull","Owl","Eagle"]
 
-@onready var vbox_container = $VBoxContainer/BirdButtonContainer
+@onready var bird_container = $VBoxContainer/ScrollContainer/BirdButtonContainer
 
 func _ready():
-	for bird in bird_names:
+	var icon_size = get_viewport().get_visible_rect().size	
+	for bird_name in bird_names:
 		var bird_button = TextureButton.new()
-		bird_button.texture_normal = load("res://Birdcalls/" + bird + "/icon.webp")
-		bird_button.pressed.connect(on_button_pressed.bind(bird))
-		vbox_container.add_child(bird_button)
+		# allow mouse input to pass through buttons, enabling touch scroll
+		bird_button.mouse_filter = Control.MOUSE_FILTER_PASS
+		bird_button.stretch_mode = TextureButton.STRETCH_SCALE
+		bird_button.custom_minimum_size = Vector2(icon_size.x/2,icon_size.x/2)
+		bird_button.ignore_texture_size = true
+		bird_button.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
+		bird_button.texture_normal = load("res://Birdcalls/" + bird_name + "/icon.webp")
+		bird_button.pressed.connect(on_button_pressed.bind(bird_name))
+		bird_container.add_child(bird_button)
 	
 func on_button_pressed(bird_name: String):
 	audio_player.stream = load("res://Birdcalls/" + bird_name + "/sound.mp3")
